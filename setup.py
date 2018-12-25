06a6043cf0b8241c38d8e06255c1038dd204032e
Shane Fontaine <shane6fontaine@gmail.com>
2018-12-25 01:26:31 -0800

    Project admin setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ethfinex",
+    version="0.0.1",
+    author="Shane Fontaine",
+    author_email="shane6fontaine@gmail.com",
+    description="A python wrapper for the Ethfinex exchange",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shanefontaine/ethfinex",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
