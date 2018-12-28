605237af4a7b7a55a8e722a180250aef96769ffc
Shane Fontaine <shane6fontaine@gmail.com>
2018-12-27 23:13:09 -0800

    Remove function annotations

--- ethfinex/public_client.py
+++ ethfinex/public_client.py
@@ -4,17 +4,17 @@ import requests
 class PublicClient(object):
     """Ethfinex public client API."""
 
-    def __init__(
-        self,
-        api_url: str = 'https://api.ethfinex.com/v2',
-        timeout: int = 30
-    ) -> None:
-        """Create Ethfinex API public client."""
+    def __init__(self, api_url='https://api.ethfinex.com/v2'):
+        """Create Ethfinex API public client.
+
+        Args:
+            api_url (Optional[str]): API URL. Defaults to etfhinex API.
+        """
         self.url = api_url.rstrip('/')
         self.auth = None
         self.session = requests.Session()
 
-    def get_platform_status(self) -> str:
+    def get_platform_status(self):
         """Get the current status of the platform.
         Returns:
             1=operative
@@ -22,8 +22,11 @@ class PublicClient(object):
         """
         return self._send_message('/platform/status')
 
-    def get_ticker(self, pair: str) -> list:
+    def get_ticker(self, pair):
         """Get a list of available currency pairs for trading.
+
+        Args:
+            pair (str): Name of the pair.
         Returns:
             [
                 BID,
@@ -40,16 +43,17 @@ class PublicClient(object):
         """
         return self._send_message(f'/ticker/{pair}')
 
-    def get_trades(
-        self,
-        pair: str,
-        limit: int = None,
-        start: int = None,
-        end: int = None,
-        sort: int = None
-    ) -> list:
+    def get_trades(self, pair, limit=None, start=None, end=None, sort=None):
         """Get all the pertinent details of the trade, such as price, size and
-        time. `sort=1` will sort results from old to new.
+        time.
+
+        Args:
+            pair (str): Name of the pair.
+            limit (Optional[int]): Number of records.
+            start (Optional[int]): Milisecond start time.
+            end (Optional[int]): Milisecond end time.
+            sort (Optional[int]): if = 1 sorts results returned with old > new
+
         Returns:
         [
             [
@@ -72,15 +76,18 @@ class PublicClient(object):
             params['sort'] = sort
         return self._send_message(f'/trades/{pair}/hist', params=params)
 
-    def get_books(
-        self,
-        pair: str,
-        precision: str,
-        len: int = None
-    ) -> list:
+    def get_books(self, pair, precision, len=None):
         """Get the state of the Bitfinex order book. It is provided on a price
-        aggregated basis, with customizable precision. Precision can be of the
-        form P0, P1, P2, P3, P4, or R0. Len can be 25 or 100.
+        aggregated basis, with customizable precision.
+
+        TODO: Clearly deefine precision and len
+
+        Args:
+            pair (str): Name of the pair.
+            precision (str): Level of price aggregation(P0, P1, P2, P3, P4, R0)
+            sort (Optional[int]): if = 1 sorts results returned with old > new
+            len (Optional[int]): Number of price points ("25", "100")
+
         Returns:
         [
             [
@@ -100,13 +107,14 @@ class PublicClient(object):
 
         return self._send_message(f'/book/{pair}/{precision}')
 
-    def _send_message(
-        self,
-        endpoint: str,
-        params: dict = None,
-        data: str = None
-    ) -> dict:
-        """Send API request."""
+    def _send_message(self, endpoint, params=None, data=None):
+        """Send API request.
+
+        Args:
+            endpoint (str): Endpoint (to be added to base URL)
+            params (Optional[dict]): HTTP request parameters
+            data (Optional[str]): JSON-encoded string payload for POST
+        """
         url = self.url + endpoint
         r = self.session.request('get', url, params=params, data=data,
                                  auth=self.auth, timeout=30)
