f2c62517009e40de4e74e1405fa7f2875d21c9eb
Shane Fontaine <shane6fontaine@gmail.com>
2018-12-28 23:03:41 -0800

    Update to 0.1.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="ethfinex-python",
-    version="0.1.1",
+    version="0.1.2",
     author="Shane Fontaine",
     author_email="shane6fontaine@gmail.com",
     license='MIT',
