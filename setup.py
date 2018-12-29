2dd9eba15e1404780f98e1d30e54203ff75e0ad7
Shane Fontaine <shane6fontaine@gmail.com>
2018-12-28 20:37:08 -0800

    Add test support

--- setup.py
+++ setup.py
@@ -1,21 +1,51 @@
-import setuptools
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+install_requires = [
+    'sortedcontainers>=1.5.9',
+    'requests>=2.13.0',
+    'six>=1.10.0',
+    'websocket-client>=0.40.0',
+    'pymongo>=3.5.1',
+]
+
+tests_require = [
+    'pytest',
+    'python-dateutil>=2.7.5',
+    ]
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="ethfinex-python",
     version="0.1.0",
     author="Shane Fontaine",
     author_email="shane6fontaine@gmail.com",
+    license='MIT',
     description="An unofficial python wrapper for the Ethfinex exchange",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/shanefontaine/ethfinex",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require={
+        'test': tests_require,
+    },
+    keywords=['ethfinex', 'ethfinex-api', 'orderbook', 'trade', 'bitcoin', 'ethereum', 'BTC', 'ETH', 'client', 'api', 'wrapper',
+              'exchange', 'crypto', 'currency', 'trading', 'trading-api', 'ethfinex-trustless', 'bitfinex'],
     classifiers=[
-        "Programming Language :: Python :: 3.6",
+        'Intended Audience :: Developers',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Intended Audience :: Information Technology',
+        'Topic :: Software Development :: Libraries :: Python Modules',
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        "Programming Language :: Python :: 3.6",
     ],
 )
